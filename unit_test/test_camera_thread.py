7dcc70a3a8a04853620fe20d83f7ea44f7914044
Khang259 <buikhang628@gmail.com>
2026-05-30 10:15:41 +0700

    THis branch used for dev test and doesn't haave function split the camera if the host has the multi GPu and doesn't have function hold enough frames to infer

--- unit_test/test_camera_thread.py
+++
@@ -1,4 +0,0 @@
-import asyncio
-from be.app.services.camera_service import get_all_bounding_boxes_by_camera
-
-print(asyncio.run(get_all_bounding_boxes_by_camera()))
--- unit_test/test_connection_to_db.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-c = MongoClient("mongodb://192.168.1.30:27017/")
-db = c["Honda_AI"]
-print(db.list_collection_names())
-print(db["node_id"].count_documents({}))
-print(db["node_id"].find_one())
